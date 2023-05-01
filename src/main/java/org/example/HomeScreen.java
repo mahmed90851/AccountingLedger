fd5bc17462763ce6fec160d66afaaaf215c21201
MohammedAhmed90851 <130098921+mahmed90851@users.noreply.github.com>
2023-05-01 13:15:51 -0400

    initial code

---
+++ src/main/java/org/example/HomeScreen.java
@@ -0,0 +1,7 @@
+package org.example;
+
+public class HomeScreen {
+    public static void main(String[] args) {
+
+    }
+}
---
+++ src/main/java/org/example/TransactionDetails.java
@@ -0,0 +1,58 @@
+package org.example;
+import java.util.Date;
+import java.time.LocalTime;
+public class TransactionDetails {
+    private Date date;
+    private LocalTime time;
+    private String description;
+    private String vendor;
+    private double amount;
+    public Date getDate() {
+        return date;
+    }
+
+    public LocalTime getTime() {
+        return time;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public String getVendor() {
+        return vendor;
+    }
+
+    public double getAmount() {
+        return amount;
+    }
+
+    public void setDate(Date date) {
+        this.date = date;
+    }
+
+    public void setTime(LocalTime time) {
+        this.time = time;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public void setVendor(String vendor) {
+        this.vendor = vendor;
+    }
+
+    public void setAmount(double amount) {
+        this.amount = amount;
+    }
+
+    public TransactionDetails(Date date, LocalTime time, String description, String vendor, double amount) {
+        this.date = date;
+        this.time = time;
+        this.description = description;
+        this.vendor = vendor;
+        this.amount = amount;
+    }
+
+}
