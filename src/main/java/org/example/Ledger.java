ca2bf5d34d88db701b11a9f7a70abdbc36350178
MohammedAhmed90851 <130098921+mahmed90851@users.noreply.github.com>
2023-05-03 14:15:06 -0400

    added notes

--- src/main/java/org/example/Ledger.java
+++ src/main/java/org/example/Ledger.java
@@ -6,14 +6,15 @@ import java.time.LocalTime;
 
 public class Ledger {
     public static Scanner scanner = new Scanner(System.in);
-
+    //the array list is made public, so it can be used throughout the program.
     private static ArrayList<TransactionDetails> transactions = readTransactions();
 
+    // this is the format for my header. it is repeated several times, so it has its own method.
     public static void printHeader() {
         System.out.printf("%-15s %-15s %-25s %-15s %-10s\n", "DATE", "TIME", "DESCRIPTION", "VENDOR", "AMOUNT");
         System.out.println("-------------------------------------------------------------------------------");
     }
-
+    // array list read's the transaction.csv file and sorts them
     public static ArrayList<TransactionDetails> readTransactions() {
 
         ArrayList<TransactionDetails> transactions = new ArrayList<>();
@@ -46,7 +47,7 @@ public class Ledger {
                 .reversed());
         return transactions;
     }
-
+    //prints all transactions that are recorded on the csv file
     public static void printAllTransactions() {
         System.out.println("============================== ALL TRANSACTIONS =============================\n");
         printHeader();
@@ -57,7 +58,8 @@ public class Ledger {
         }
         ApplicationInterface.showLedgerScreen();
     }
-
+    // prints out the deposits by selecting the amount value's that are greater than 0.
+    // all deposits have a positive value.
     public static void printDeposits() {
         System.out.println("============================== ALL DEPOSITS ============================== \n");
         printHeader();
@@ -70,7 +72,8 @@ public class Ledger {
         }
         ApplicationInterface.showLedgerScreen();
     }
-
+    // prints out the payments by selecting the amount value's that are less than 0.
+    // all payments have a negative value.
     public static void printPayments() {
         System.out.println("================================ ALL PAYMENTS ===============================\n");
         printHeader();
@@ -83,7 +86,7 @@ public class Ledger {
         }
         ApplicationInterface.showLedgerScreen();
     }
-
+    // this screen allows the user to show the report of the ledger. they can request specific transactions to be displayed
     public static void showReport() {
         ArrayList<TransactionDetails> transactions = new ArrayList<TransactionDetails>();
         Scanner scanner = new Scanner(System.in);
@@ -132,13 +135,15 @@ public class Ledger {
                 break;
         }
     }
-
+    // this method displays month to date transactions.
+    // it prints out all transactions that match the current local month
     public static void getMonthToDate() {
         LocalDate today = LocalDate.now();
 
         System.out.println("\n================================ Month to Date ================================");
         printHeader();
-
+        //this for loop compares the transaction's month and year to the current one and displays them if they match.
+        // the comparison of the year is important because it prevents the program from printing the same current month from previous years.
         for (TransactionDetails transaction : transactions) {
             LocalDate transactionDate = transaction.getDate();
             if (transactionDate.getMonth() == today.getMonth() && transactionDate.getYear() == today.getYear()) {
@@ -166,7 +171,7 @@ public class Ledger {
             }
         }
     }
-
+    // prints all transactions that contain the year that we are currently in.
     public static void getYearToDate() {
         LocalDate today = LocalDate.now();
 
@@ -182,7 +187,7 @@ public class Ledger {
         }
     }
 
-
+    // prints all transactions from the previous year by printing transactions for this year minus 1
     public static void getPreviousYear() {
         LocalDate today = LocalDate.now();
 
@@ -197,7 +202,7 @@ public class Ledger {
             }
         }
     }
-
+    // searches the list by comparing the user input to the vendors on the list. if the user inputs match it will print out the transactions.
     public static void searchByVendor() {
 
         System.out.print("Enter vendor name: ");
@@ -219,6 +224,7 @@ public class Ledger {
     public static void customSearch() {
         /* Prompt the user for search values
            if the user wants to skip the search value they can press enter.
+           the if statement makes it so that if there is no input from the user it can move on
          */
         System.out.print("Enter start date (yyyy-mm-dd, or press Enter to skip): ");
         String startDateInput = scanner.nextLine().trim();
@@ -238,7 +244,6 @@ public class Ledger {
             endDate = LocalDate.parse(endDateInput);
         }
 
-
         System.out.print("Enter description (or press Enter to skip): ");
         String description = scanner.nextLine().trim();
 
@@ -256,11 +261,16 @@ public class Ledger {
         System.out.println("\n============================== SEARCH RESULTS ==============================");
         printHeader();
         for (TransactionDetails transaction : transactions) {
+            // this checks to see if the start date for the transaction is NOT before the date entered by the user
             boolean matchesStartDate = startDate == null || !transaction.getDate().isBefore(startDate);
+            // this checks to see if the end date for the transaction is NOT after the date entered by the user
             boolean matchesEndDate = endDate == null || !transaction.getDate().isAfter(endDate);
+            // checks to see if users description input contains the description from the list
             boolean matchesDescription = description.isEmpty() || transaction.getDescription().toLowerCase().contains(description.toLowerCase());
+            // checks to see if users vendor input contains the vendor from the list
             boolean matchesVendor = vendor.isEmpty() || transaction.getVendor().toLowerCase().contains(vendor.toLowerCase());
-            boolean matchesAmount = amount == 0 || Math.abs(transaction.getAmount() - amount) < 0.001;
+            // checks to see if the users amount input matches any amount from the list
+            boolean matchesAmount = amount == 0 || transaction.getAmount() == amount;
 
             if (matchesStartDate && matchesEndDate && matchesDescription && matchesVendor && matchesAmount) {
                 System.out.printf("%-15s %-15s %-25s %-15s %-10.2f\n", transaction.getDate(), transaction.getTime(), transaction.getDescription(),
